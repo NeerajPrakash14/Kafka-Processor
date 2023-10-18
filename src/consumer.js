e1992b34302b899662638ee76f80505852eedc18
Neeraj Prakash <neerajprakashreddy@gmail.com>
2023-10-18 21:53:50 +0530

    Update consumer.js

--- src/consumer.js
+++ src/consumer.js
@@ -1,26 +1,46 @@
 const {kafka} = require('./main')
-const {publisher, subscriber} = require('./pub-sub')
+//const {publisher, subscriber} = require('./pub-sub')
 
 
 const run = async (consumer) => {
     await consumer.connect();
     await consumer.subscribe({ topic: "topic1", fromBeginning: true });
   
+    // await consumer.run({
+    //   eachMessage: async ({ topic, partition, message }) => {
+    //     // Process each consumed message
+    //     console.log({
+    //       topic,
+    //       partition,
+    //       off set: message.offset,
+    //       value: message.value.toString(),
+    //     });
+
+    //     // // Publish a message
+    //     // await publisher.publish('channel1', 'Hello, subscribers!');
+
+    //   },
+    // });
+
+
+    console.time("single_consumer");
     await consumer.run({
-      eachMessage: async ({ topic, partition, message }) => {
-        // Process each consumed message
-        console.log({
-          topic,
-          partition,
-          offset: message.offset,
-          value: message.value.toString(),
-        });
-
-        // Publish a message
-        await publisher.publish('channel1', 'Hello, subscribers!');
-
-      },
-    });
+        eachBatchAutoResolve: false,
+        eachBatch: async ({ batch, resolveOffset, heartbeat, isRunning, isStale }) => {
+            //console.log('Batch -> ', batch);
+            for (let message of batch.messages) {
+                if (!isRunning() || isStale()) break
+                console.log({
+                    offset: message.offset,
+                    value: message.value.toString(),
+                  });
+                resolveOffset(message.offset)
+                await heartbeat()
+            }
+        },
+        //maxBatchSize: 10
+    })
+    console.timeEnd("single_consumer");
 };
 
 
