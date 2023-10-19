c6a287d00500a2082c8d08caea39ae47dd11f580
Neeraj Prakash <neerajprakashreddy@gmail.com>
2023-10-19 23:03:18 +0530

    Update consumer_cp.js

--- src/consumer_cp.js
+++ src/consumer_cp.js
@@ -1,31 +1,77 @@
 const {kafka} = require('./main')
-//const {publisher, subscriber} = require('./pub-sub')
+const {publisher, subscriber} = require('./pub-sub_cp')
+
+const CLEANUP_INTERVAL = 1 * 1000; // in milliseconds
+
+let messages = [];
+let summary = {"msg_count": 0};
 
 
-const run = async (consumer) => {
+
+const consume_msg = async (consumer) => {
     await consumer.connect();
-    await consumer.subscribe({ topic: "topic1", fromBeginning: true });
+    await consumer.subscribe({ topic: "app1", fromBeginning: true });
   
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         // Process each consumed message
-        console.log({
-          topic,
-          partition,
-          offset: message.offset,
-          value: message.value.toString(),
-        });
+        // console.log({
+        //   topic,
+        //   partition,
+        //   offset: message.offset,
+        //   value: message.value.toString(),
+        // });
 
-        // // Publish a message
-        // await publisher.publish('channel1', 'Hello, subscribers!');
+        processMessage(message);
 
       },
     });
 };
 
 
-const consumer1 = kafka.consumer({ groupId: 'cgroup1' })
-run(consumer1).catch(console.error);
+function startConsumers(consumerCount){
+    for(let i=0;i<consumerCount;i++){
+        const consumer = kafka.consumer({ groupId: 'cgroup1' })
+        consume_msg(consumer).catch(console.error);
+    }
+}
 
-// const consumer2 = kafka.consumer({ groupId: 'cgroup2' })
-// run(consumer2).catch(console.error);
+startConsumers(10);
+
+// consumer lag :
+    // producers - 1000
+    // Topic traffic - 1000 * 1000 = 1 M /s
+    // consumer - 100
+    // 1 con consumes nearly - 5000 /s
+    // 100 con consumes nearly - 1 M /
+    // lag - 2 
+    // 
+    // each service 
+
+// const consumer1 = kafka.consumer({ groupId: 'cgroup1' })
+// run(consumer1).catch(console.error);
+
+function processMessage(message) {
+    messages.push(message);
+    summary[message['value']] = 1;
+    summary['msg_count'] = summary['msg_count'] + 1;
+}
+
+
+// Interval Batch Publish to pub-sub channel
+function cleanup(){
+    messages = [];
+    summary = {"msg_count": 0};
+}
+
+function publishSummary(){
+    setInterval(async () => {
+
+        publisher.publish('channel1', JSON.stringify(summary));
+        cleanup();
+       
+    }, CLEANUP_INTERVAL)
+}
+publishSummary();
+
+
